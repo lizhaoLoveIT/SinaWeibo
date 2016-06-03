1054fb31dbd512c09b1b9b86d2b1a792f8c39025
lizhao <605482314@qq.com>
2016-06-03 14:59:11 +0800

    1

---
+++ sinaWeiBo/sinaWeiBoTests/sinaWeiBoTests.swift
@@ -0,0 +1,36 @@
+//
+//  sinaWeiBoTests.swift
+//  sinaWeiBoTests
+//
+//  Created by 李朝 on 16/6/3.
+//  Copyright © 2016年 IFengXY. All rights reserved.
+//
+
+import XCTest
+@testable import sinaWeiBo
+
+class sinaWeiBoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
